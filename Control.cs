8838cbf3c7c9615adf2594e945ffeafb4ae18567
Abdullah <atariq51@gmail.com>
2021-10-01 16:23:20 +0200

    Circolor Scripts
    Three scripts from Circolor.
    Control.cs for player rotation functionality testing in editor.
    EnemyMove.cs for enemy movement.
    mobilecontrol.cs for player input on mobile devices through touch input.

---
+++ Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	void Start () {
+
+        Invoke("disableanimation", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () //Basic Rotation functionality
+	{
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate(new Vector3(0,0,1), -300f * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Rotate(new Vector3(0,0,1), 300f * Time.deltaTime);
+		}
+	}
+
+    void disableanimation()  //Disables ring rotation animation 1 second after beign created
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+}
---
+++ mobilecontrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobilecontrol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()   
+	{
+		foreach (Touch touch in Input.touches) {
+			if (touch.position.x < Screen.width/2)  //Rotate according to touch position
+			{                                        
+				transform.Rotate(new Vector3(0,0,1), 300f * Time.deltaTime);
+			}
+			else if (touch.position.x > Screen.width/2)
+			{
+				transform.Rotate(new Vector3(0,0,1), -300f * Time.deltaTime);
+			}
+
+		}
+	}
+}
